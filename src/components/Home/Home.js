10de4bd3504caa39823408c7d5894b672b971a01
Kohnodker Khademul Bari <bari35-1159@diu.edu.bd>
2021-08-08 01:44:56 +0600

    Added Club Componet and home team list view done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Club from '../Club/Club';
 import Header from '../Header/Header';
+import './Home.css'
 
 const Home = () => {
+    const[teams,setTeams] = useState([])
+    useEffect(()=>{
+        const url = 'https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League'
+        fetch(url)
+        .then(response=>response.json())
+        .then(data=>setTeams(data.teams))
+    },[])
     return (
+        <div className="home">
         <div>
             <Header></Header>
-            <h3>this is from home</h3>
         </div>
+        <div className="team-list">
+            {
+                teams.map(team=><Club key={team.idTeam} teams={team}></Club>)
+            }
+        </div>
+        </div>
+     
     );
 };
 
